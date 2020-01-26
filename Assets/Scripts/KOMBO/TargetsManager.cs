e18cf4ff6c83b4fe2bc1c454488b425f02c309dc
Sklepper <klepper.samuel@gmail.com>
2020-01-26 17:52:36 +0100

    Update TargetsManager.cs

--- Assets/Scripts/KOMBO/TargetsManager.cs
+++ Assets/Scripts/KOMBO/TargetsManager.cs
@@ -74,9 +74,9 @@ namespace Hitbox.Kombo
     public class TargetsManager : MonoBehaviour
     {
         public GameObject targetPrefab ;
-        List<GameObject> targetsList;       // list of current targets
-        List<Color> _reachTargetsColor;      // colors of reached targets
-        List<Vector3> _reachTargetsPosition;   // position of reached targets
+        List<GameObject> targetsList;			// list of current targets
+        List<Color> _reachTargetsColor;			// colors of reached targets
+        List<Vector3> _reachTargetsPosition;	// position of reached targets
 
         [SerializeField]
         private TargetProperties _targetPropLvl1 = new TargetProperties(3.0f, 40.0f, 85.0f, 0.0f);      // RGB
@@ -112,137 +112,134 @@ namespace Hitbox.Kombo
             _comboMultiply *= 2;
         }
 
-        public void GetImpact(Vector2 position2D_)
-        {
-            Vector3 position3D_ = new Vector3(position2D_.x, position2D_.y, this.gameObject.transform.position.z + 50f);
+		public void GetImpact(Vector2 position2D_)
+		{
+			/// Get position and set raycast vector
+			Vector3 position3D_ = new Vector3(position2D_.x, position2D_.y, this.gameObject.transform.position.z + 50f);
+			Vector3 cameraForward = _hitboxCamera.transform.forward;
+			Debug.DrawRay(position2D_, cameraForward * 10000, Color.yellow, 10.0f);
 
-            Vector3 cameraForward = _hitboxCamera.transform.forward;
-            Debug.DrawRay(position2D_, cameraForward * 10000, Color.yellow, 10.0f);
+			/// Raycast target and action
+			Color targetColor_ = Color.black;
+			if (targetsList.Count > 0)
+			{
+				RaycastHit hit;
+				if (Physics.Raycast(position2D_, cameraForward, out hit))
+				{
+					if (hit.collider != null && hit.transform.tag == "target")
+					{
+						targetColor_ = hit.collider.GetComponent<TargetBehavior>().GetColor();
+						hit.collider.GetComponent<TargetBehavior>().SetHit();
+						SetTargetsFromTarget(position3D_, targetColor_); // Action : set new targets from the one reached
+					}
+				}
+			}
+		}
 
-            bool isTrigger_ = false ;
-            Color targetColor_ = Color.black;
+		void SetTargetsFromTarget(Vector3 posTargt_, Color colTargt_)
+		{
+            Color[] colTargets_;
+
             if (targetsList.Count > 0)
             {
-                RaycastHit hit;
-                if (Physics.Raycast(position2D_, cameraForward, out hit))
+                _reachTargetsColor.Add(colTargt_);		// update reached target colors
+                _reachTargetsPosition.Add(posTargt_);	// update reached target positions
+
+                if (colTargt_ == Color.red)
                 {
-                    if (hit.collider != null && hit.transform.tag == "target")
-                    {
-                        targetColor_ = hit.collider.GetComponent<TargetBehavior>().GetColor();
-                        hit.collider.GetComponent<TargetBehavior>().SetHit();
-                        isTrigger_ = true;
-                    }
+                    colTargets_ = new Color[2];
+                    colTargets_[0] = Color.magenta;
+                    colTargets_[1] = Color.yellow;
+                    SetCrownTargets(posTargt_, colTargets_, _targetPropLvl2);
+
+                    _score += 2 * _comboMultiply;
+                    _comboMultiply *= 4;
                 }
-            }
-            else {
-                isTrigger_ = true;
-            }
 
-            if (isTrigger_)
-            {
-                Color[] colTargets_;
-
-                if (targetsList.Count > 0)
+                if (colTargt_ == Color.green)
                 {
-                    _reachTargetsColor.Add(targetColor_);   // update reached target colors
-                    _reachTargetsPosition.Add(position3D_); // update reached target positions
+                    colTargets_ = new Color[2];
+                    colTargets_[0] = Color.yellow;
+                    colTargets_[1] = Color.cyan;
+                    SetCrownTargets(posTargt_, colTargets_, _targetPropLvl2);
 
-                    if (targetColor_ == Color.red)
-                    {
-                        colTargets_ = new Color[2];
-                        colTargets_[0] = Color.magenta;
-                        colTargets_[1] = Color.yellow;
-                        SetCrownTargets(position3D_, colTargets_, _targetPropLvl2);
+                    _score += 2 * _comboMultiply;
+                    _comboMultiply *= 4;
+                }
 
-                        _score += 2 * _comboMultiply;
-                        _comboMultiply *= 4;
-                    }
+                if (colTargt_ == Color.blue)
+                {
+                    colTargets_ = new Color[2];
+                    colTargets_[0] = Color.cyan;
+                    colTargets_[1] = Color.magenta;
+                    SetCrownTargets(posTargt_, colTargets_, _targetPropLvl2);
 
-                    if (targetColor_ == Color.green)
-                    {
-                        colTargets_ = new Color[2];
-                        colTargets_[0] = Color.yellow;
-                        colTargets_[1] = Color.cyan;
-                        SetCrownTargets(position3D_, colTargets_, _targetPropLvl2);
+                    _score += 2 * _comboMultiply;
+                    _comboMultiply *= 4;
+                }
 
-                        _score += 2 * _comboMultiply;
-                        _comboMultiply *= 4;
-                    }
+                if (colTargt_ == Color.white)
+                {
+                    _score += 4 * _comboMultiply;
+                    _comboMultiply *= 8;
+                }
 
-                    if (targetColor_ == Color.blue)
-                    {
-                        colTargets_ = new Color[2];
-                        colTargets_[0] = Color.cyan;
-                        colTargets_[1] = Color.magenta;
-                        SetCrownTargets(position3D_, colTargets_, _targetPropLvl2);
+                if (colTargt_ == Color.yellow || colTargt_ == Color.cyan || colTargt_ == Color.magenta)
+                {
+                    int[] indexCMJ_ = new int[3];
+                    bool isYellow_ = false;
+                    bool isCyan_ = false;
+                    bool isMagenta_ = false;
+                    for (int i = 0; i < _reachTargetsColor.Count; i++) {
+                        if (_reachTargetsColor[i] == Color.yellow && !isYellow_) {
+                            indexCMJ_[0] = i;                                       // get yellow index
+                            isYellow_ = true;
+                        }
 
-                        _score += 2 * _comboMultiply;
-                        _comboMultiply *= 4;
-                    }
+                        if (_reachTargetsColor[i] == Color.cyan && !isCyan_){
+                            indexCMJ_[1] = i;                                       // get cyan index
+                            isCyan_ = true;
+                        }
 
-                    if (targetColor_ == Color.white)
-                    {
-                        _score += 4 * _comboMultiply;
-                        _comboMultiply *= 8;
-                    }
+                        if (_reachTargetsColor[i] == Color.magenta && !isMagenta_) {
+                            indexCMJ_[2] = i;                                       // get magenta index
+                            isMagenta_ = true;
+                        }                                
 
-                    if (targetColor_ == Color.yellow || targetColor_ == Color.cyan || targetColor_ == Color.magenta)
-                    {
-                        int[] indexCMJ_ = new int[3];
-                        bool isYellow_ = false;
-                        bool isCyan_ = false;
-                        bool isMagenta_ = false;
-                        for (int i = 0; i < _reachTargetsColor.Count; i++) {
-                            if (_reachTargetsColor[i] == Color.yellow && !isYellow_) {
-                                indexCMJ_[0] = i;                                       // get yellow index
-                                isYellow_ = true;
+                        if (isYellow_ && isCyan_ && isMagenta_) {
+                            float xG_ = 0.0f;
+                            for (int j = 0; j < 3; j++)
+                            {
+                                xG_ += _reachTargetsPosition[indexCMJ_[j]].x;
+                            }
+                            xG_ /= 3.0f; // get X pos of white target
+
+                            float yG_ = 0.0f;
+                            for (int j = 0; j < 3; j++)
+                            {
+                                yG_ += _reachTargetsPosition[indexCMJ_[j]].y;
+                            }
+                            yG_ /= 3.0f;  // get Y pos of white target
+
+                            float zG_ = 0.0f;
+                            for (int j = 0; j < 3; j++)
+                            {
+                                zG_ += _reachTargetsPosition[indexCMJ_[j]].z;
+                            }
+                            zG_ /= 3.0f;  // get Z pos of white target
+
+                            // Set white target
+                            Vector3 posWhite_ = new Vector3(xG_, yG_, zG_);
+                            SetTarget(posWhite_, Color.white, 0.0f, _targetPropLvl3);
+
+                            // Destroyed reached targets
+                            System.Array.Sort(indexCMJ_);
+                            for (int j = 2; j >= 0; j--) {
+                                _reachTargetsColor.RemoveAt(indexCMJ_[j]);
+                                _reachTargetsPosition.RemoveAt(indexCMJ_[j]);                                    
                             }
 
-                            if (_reachTargetsColor[i] == Color.cyan && !isCyan_){
-                                indexCMJ_[1] = i;                                       // get cyan index
-                                isCyan_ = true;
-                            }
-
-                            if (_reachTargetsColor[i] == Color.magenta && !isMagenta_) {
-                                indexCMJ_[2] = i;                                       // get magenta index
-                                isMagenta_ = true;
-                            }                                
-
-                            if (isYellow_ && isCyan_ && isMagenta_) {
-                                float xG_ = 0.0f;
-                                for (int j = 0; j < 3; j++)
-                                {
-                                    xG_ += _reachTargetsPosition[indexCMJ_[j]].x;
-                                }
-                                xG_ /= 3.0f; // get X pos of white target
-
-                                float yG_ = 0.0f;
-                                for (int j = 0; j < 3; j++)
-                                {
-                                    yG_ += _reachTargetsPosition[indexCMJ_[j]].y;
-                                }
-                                yG_ /= 3.0f;  // get Y pos of white target
-
-                                float zG_ = 0.0f;
-                                for (int j = 0; j < 3; j++)
-                                {
-                                    zG_ += _reachTargetsPosition[indexCMJ_[j]].z;
-                                }
-                                zG_ /= 3.0f;  // get Z pos of white target
-
-                                // Set white target
-                                Vector3 posWhite_ = new Vector3(xG_, yG_, zG_);
-                                SetTarget(posWhite_, Color.white, 0.0f, _targetPropLvl3);
-
-                                // Destroyed reached targets
-                                System.Array.Sort(indexCMJ_);
-                                for (int j = 2; j >= 0; j--) {
-                                    _reachTargetsColor.RemoveAt(indexCMJ_[j]);
-                                    _reachTargetsPosition.RemoveAt(indexCMJ_[j]);                                    
-                                }
-
-                                break;
-                            }
+                            break;
                         }
                     }
                 }
